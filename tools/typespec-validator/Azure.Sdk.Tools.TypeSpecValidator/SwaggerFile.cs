208f55e6026cf6931f79ede9fecc66fb8790ecb6
Mike Harder <mharder@microsoft.com>
2023-03-24 00:12:48 -0700

    Code cleanup

--- tools/typespec-validator/Azure.Sdk.Tools.TypeSpecValidator/SwaggerFile.cs
+++ tools/typespec-validator/Azure.Sdk.Tools.TypeSpecValidator/SwaggerFile.cs
@@ -1,10 +1,7 @@
 using System;
-using System.Collections;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.IO;
 using System.Linq;
-using System.Text.Json;
 using System.Text.Json.Nodes;
 using Microsoft.Extensions.FileSystemGlobbing;
 
